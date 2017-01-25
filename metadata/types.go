c4feefc36fccd34f21a62d71aa3d68a525f7dd16
Alena Prokharchyk <alena@rancher.com>
2017-01-25 11:42:13 -0800

    standalone container to lb
    healthcheck to container

--- metadata/types.go
+++ metadata/types.go
@@ -71,6 +71,7 @@ type Container struct {
 	Links                    map[string]string `json:"links"`
 	System                   bool              `json:"system"`
 	EnvironmentUUID          string            `json:"environment_uuid"`
+	HealthCheck              HealthCheck       `json:"health_check"`
 }
 
 type Network struct {
@@ -106,6 +107,7 @@ type PortRule struct {
 	Priority    int    `json:"priority"`
 	BackendName string `json:"backend_name"`
 	Selector    string `json:"selector"`
+	Container   string `json:"container"`
 }
 
 type LBConfig struct {
